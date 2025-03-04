24bd3b5470138ac390e3fd456ef0f45f9d6d24da
ApichayaUWU <apichaya.inkhiewsai@gmail.com>
2025-03-04 21:21:09 +0700

    add : env

--- database/redis.go
+++ database/redis.go
@@ -2,16 +2,22 @@ package database
 
 import (
 	"context"
-	"github.com/redis/go-redis/v9"
 	"log"
+	"os"
+
+	"github.com/redis/go-redis/v9"
 )
 
 var RedisClient *redis.Client
 var ctx = context.Background()
 
 func ConnectRedis() {
+	addr := os.Getenv("REDIS_ADDR")
+	if addr == "" {
+		addr = "localhost:6379"
+	}
 	RedisClient = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379", // Change if Redis is hosted elsewhere
+		Addr:     addr, // Change if Redis is hosted elsewhere
 		Password: "",               // No password by default
 		DB:       0,                // Use default DB
 	})
